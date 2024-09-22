a82b7fd50cd2bf00bff79c4cc27684fdad9c9c4b
Whebon <bart-man99@hotmail.com>
2024-09-22 12:59:21 +0200

    Add getAnimalfolkIds

--- daleofmerchants.game.php
+++ daleofmerchants.game.php
@@ -191,8 +191,7 @@ class DaleOfMerchants extends DaleTableBasic
         $result['cardTypes'] = $this->card_types;
         $result['effects'] = $this->effects->loadFromDb();
         $result['inDeckSelection'] = $this->getGameStateValue("inDeckSelection") == '1';
-        $result['animalfolkIds'] = array();
-        //$result['animalfolkIds'] = $result['inDeckSelection'] ? array() : $this->deckSelection->getAnimalfolkIds();
+        $result['animalfolkIds'] = $result['inDeckSelection'] ? array() : $this->deckSelection->getAnimalfolkIds();
         $result['debugMode'] = $this->getGameStateValue("debugMode") == '1';
   
         return $result;
